a3561563ee6cdc5111f2734a756e477fcfc9f5c4
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2023-12-29 00:13:49 +0100

    implement camera and microphone toggle

--- src/Call/Jsx/components.jsx
+++ src/Call/Jsx/components.jsx
@@ -8,7 +8,8 @@ import { AgoraRTCProvider
         , useRemoteAudioTracks
         , useRemoteUsers
         , RemoteUser
-        , LocalVideoTrack } from "agora-rtc-react";
+        , LocalVideoTrack, 
+        LocalUser} from "agora-rtc-react";
 import AgoraRTC from "agora-rtc-sdk-ng";
 import { Button } from "@mui/material";
 
@@ -16,7 +17,7 @@ import { Button } from "@mui/material";
 
 export default function StreamMain ({channel}){
     const [appId,setAppId] = useState("945e0c1e774946de9c2e9a599f8c9c84");
-    const [token,setToken] = useState("007eJxTYLh7Xz9syrnKtosTLHyu/qh5pjCX98Zeya2X1kkcmfQ1wYpbgcHSxDTVINkw1dzcxNLELCXVMtko1TLR1NIyzSLZMtnC5Elxd2pDICPDlV4fRkYGCATxWRhyEzPzGBgA+xohrQ==");
+    const [token,setToken] = useState("007eJxTYDjN4DYr/dvUHwfrLRKa9gVX3pnh+1f18plsx9MucaUKTQcVGCxNTFMNkg1Tzc1NLE3MUlItk41SLRNNLS3TLJItky1M+P/1pjYEMjLcjJnEysgAgSA+C0NuYmYeAwMAwk8hBA==");
     const client = useRTCClient(AgoraRTC.createClient({mode:"rtc",codec:"vp8"}));
         return <AgoraRTCProvider client={client} >
                     <Streams channel={channel} appId={appId} token={token} />
@@ -32,36 +33,62 @@ function Streams({appId, channel, token}){
     const remoteUsers = useRemoteUsers();
     const {audioTracks} = useRemoteAudioTracks(remoteUsers);
     audioTracks.map(function(track){ return track.play()});
+
+    const [playVideo, setPlayVideo] = useState(true);
+    const [audioState, setAudioState] = useState(true);
         
    return <div className="w-screen h-screen" >
                 {deviceLoading && <Loading/> }
-                {remoteUsers.map((remoteUser) =>  <RemoteStream id={remoteUser.uid} user={remoteUser} playVideo={true} playAudio={true} />)}
-                {!deviceLoading && <LocalStream track={VideoTrack.localCameraTrack} play ={true} muted = {true}/>}
-                <StreamControls/>
+                <div className="w-screen h-1/5 flex gap-4 px-4 justify-center" >
+                    {remoteUsers.map((remoteUser) =>  <RemoteStream id={remoteUser.uid} user={remoteUser} playVideo={true} playAudio={true} />)}
+                </div>
+                {!deviceLoading && <div className="w-full h-3/4 flex items-center justify-center pt-8 ">
+                                        <div className="w-3/4 h-full">
+                                            <LocalUser videoTrack={VideoTrack.localCameraTrack} audioTrack={AudioTrack.localMicrophoneTrack} playAudio playVideo cameraOn  = {playVideo} micOn = {audioState} />
+                                        </div>
+                                    </div>
+                }
+                <StreamControls camFun={setPlayVideo} micFun={setAudioState} />
     </div>
 }
 
 function RemoteStream({id, user, playVideo, playAudio}){
-    return <div style={{ height: 300, width: 600 }} key={id}>
-                <RemoteUser user={user} playVideo={playVideo} playAudio={true} />
+    return <div className="w-1/4 h-full" key={id}>
+                <RemoteUser user={user} playVideo={playVideo} playAudio={playAudio} />
         </div>
-}
-
-function LocalStream({track, play, muted}){
-    return <div className="w-full h-3/4 flex items-center justify-center ">
-        <div className="w-3/4 h-full">
-            <LocalVideoTrack track={track} play = {play} muted = {muted} />
-        </div>
-    </div>
 }
 
 function Loading(){
     return <div>loading component</div>
 }
-function StreamControls(){
-    return <div>
 
+function StreamControls({micFun,camFun}){
+
+    function ToggleMic(){
+        micFun(initial => !initial)
+    }
+    function ToggleCam(){
+        camFun(initial => !initial)
+    }
+
+    return <div className="w-full flex justify-center gap-4">
+        <Button onClick={ToggleMic} >Mic</Button>
+        <Button onClick={ToggleCam}>Camera</Button>
     </div>
 }
+
+
+
+function LocalStream({track, play, muted}){
+    return <div className="w-full h-3/4 flex items-center justify-center ">
+        <div className="w-3/4 h-full">
+            {/* <LocalVideoTrack track={track} play = {play} muted = {muted} /> */}
+            <LocalVideoTrack track={VideoTrack.localCameraTrack} play ={true} muted = {false}/>
+        </div>
+    </div>
+}
+
+
+
 
 

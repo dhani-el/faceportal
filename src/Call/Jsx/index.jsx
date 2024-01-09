ff2c22045492266a1d85532891fb1e7b2bb6264d
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-01-09 02:13:59 +0100

    call screen basic layout

--- src/Call/Jsx/index.jsx
+++ src/Call/Jsx/index.jsx
@@ -7,8 +7,7 @@ import StreamMain from "./components";
 export default function Call(){
     const params = useParams();
     console.log(params);
-    return <div>
-        <div>Face Portal</div>
+    return <div className="w-screen h-screen overflow-hidden grid grid-cols-4 " >
         <StreamMain channel={params.channel} uid={params.uid} />
     </div>
 }

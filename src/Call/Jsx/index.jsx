0ae8fdc1c2a7b82e12edd9b94e029b946d4135b9
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-01-12 05:46:32 +0100

    basic socket connection

--- src/Call/Jsx/index.jsx
+++ src/Call/Jsx/index.jsx
@@ -8,10 +8,10 @@ export default function Call(){
     const params = useParams();
     console.log(params);
     return <div className="w-screen h-screen overflow-hidden landscape:flex landscape:justify-around " >
-        <StreamMain channel={params.channel} uid={params.uid} />
-        {/* <div className="w-full landscape:w-8/12 relative h-full flex flex-col justify-around items-center landscape:px-6 border-yellow-200 border-solid border-2 ">
+        {/* <StreamMain channel={params.channel} uid={params.uid} /> */}
+        <div className="w-full landscape:w-8/12 relative h-full flex flex-col justify-around items-center landscape:px-6 border-yellow-200 border-solid border-2 ">
             dummy video call replacement
-        </div> */}
+        </div>
         <ChatNParticipant/>
     </div>
 }

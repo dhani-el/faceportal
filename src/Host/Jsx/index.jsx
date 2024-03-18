e36f715fa4cea66617f64375e4d4f0d52bde61c3
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2024-03-18 17:50:22 +0100

    implement visible feed back after meeting link is copied

--- src/Host/Jsx/index.jsx
+++ src/Host/Jsx/index.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { TextField, Button, InputAdornment } from "@mui/material";
 import { CopyAllRounded } from "@mui/icons-material";
@@ -16,6 +16,8 @@ export default function Host(){
     const [customLink, setCustomLink] = useState('');
     const [autoLink, setAutoLink] = useState('');
     const [link, setLink] = useState("");
+    const CopyFieldRef = useRef(null);
+    const copyContainerRef = useRef(null);
     const channelIdLength = 6;
 
     function inputValueChange(setterFunction, text){
@@ -72,24 +74,33 @@ export default function Host(){
     function goToMeeting(){
         navigate(link);
     }
-    function Copy(){
-        navigator.clipboard.writeText(autoLink)
+    function handleCopyClicked(){
+        if (autoLink == "") {
+            return
+        }
+        navigator.clipboard.writeText(autoLink);
+        CopyFieldRef.current.value = "Copied ! ! !";
+        copyContainerRef.current.classList.add("copied");
+        if(CopyFieldRef != null){
+            setTimeout(function(){
+                copyContainerRef.current.classList.remove("copied");
+                CopyFieldRef.current.value = autoLink;
+            },1000)
+        }
     }
 
     return <motion.div id="container">
-                    <p className="pt-16" >HOST</p>
+                    <p className="pt-16 text-2xl font-montserrat font-semibold" >HOST</p>
                     <motion.div id="hostForm">
                     <TextField  onChange={(e)=>inputValueChange(setDisplayName,e.currentTarget.value)} value={displayName} placeholder="Your Display Name" variant="filled" autoComplete="false" sx={{paddingBottom:"1.5rem",}}/>
-                    <TextField   onChange={(e)=>inputValueChange(setCustomLink,e.currentTarget.value)}  value={customLink} placeholder="Enter Custom Link" variant="filled" disabled = {autoGenerate} sx={{paddingBottom:"1.5rem",}}/>
+                    <TextField   onChange={(e)=>inputValueChange(setCustomLink,e.currentTarget.value)}  value={customLink} placeholder="Enter Custom ID" variant="filled" disabled = {autoGenerate} sx={{paddingBottom:"1.5rem",}}/>
                     <motion.div id="buttonContainers">
-                        <Button onClick={GenerateCustomLinkNav} variant="contained" >Generate Custom Link</Button>
-                        <Button onClick={autoGenerateLinkNav} variant="contained" >Auto Generate Link</Button>
+                        <Button onClick={GenerateCustomLinkNav} variant="contained" sx={{backgroundColor:"darkcyan",color:"white",fontFamily:"montserrat",fontWeight:"600"}} className="hover:bg-cyan-800"  >Generate Custom ID</Button>
+                        <Button onClick={autoGenerateLinkNav} variant="contained" sx={{backgroundColor:"darkcyan",color:"white",fontFamily:"montserrat",fontWeight:"600"}}  >Auto Generate ID</Button>
                     </motion.div>
                     <motion.div id="copyNGo" className="w-Screen landscape:flex landscape:items-center landscape:flex-col landscape:gap-6 " >
-                        <TextField value={autoLink} placeholder="Copy Link" sx={{paddingTop:"3rem",}} className="p-0" InputProps={{endAdornment:(
-                            <InputAdornment  position="start"  ><CopyAllRounded className="hover:cursor-pointer" onClick = {Copy()}  /></InputAdornment>
-                        )}} inputProps={{readOnly:"true"}} />
-                        <Button onClick={goToMeeting} disabled = {link === "" ? true : false} variant="contained" className="w-fit" >Go to Meeting</Button>
+                        <TextField ref={copyContainerRef} inputRef={CopyFieldRef} value={autoLink} placeholder="Copy Link" sx={{paddingTop:"3rem"}} className="p-0 font-montserrat" InputProps={{endAdornment:(<InputAdornment  position="start" sx={{height:"100%"}} ><Button variant="text"  onClick = {handleCopyClicked} sx={{color:"black", borderColor:"black",}} ><CopyAllRounded  className="hover:cursor-pointer focus:bg-transparent" /></Button></InputAdornment>)}} inputProps={{readOnly:"true"}} />
+                        <Button  onClick={goToMeeting} disabled = {link === "" ? true : false} variant="contained" className="w-fit" sx={{backgroundColor:"darkcyan",color:"white",fontFamily:"montserrat",fontWeight:"600"}} >Go to Meeting</Button>
                     </motion.div>
                     </motion.div>
             </motion.div>
